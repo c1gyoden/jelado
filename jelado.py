893efb73cf7207aab73a3d73216491480a12a79f
c1gyoden <c1gyoden@vasvari.org>
2024-05-03 11:25:22 +0200

    2. feladat

--- jelado.py
+++ jelado.py
@@ -14,3 +14,6 @@ for sor in fajl:
     sor = sor.strip().split()
     jelek.append(Jel(sor[0], sor[1], sor[2], sor[3], sor[4]))
 
+print("2. feladat")
+sorszam = int(input("Adja meg a jel sorszámát! ")) - 1
+print(f'x={jelek[sorszam].x} y={jelek[sorszam].y}')
