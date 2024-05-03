ea515fc6b0dd50632bf307fe7cfac3616655ab80
c1gyoden <c1gyoden@vasvari.org>
2024-05-03 13:32:58 +0200

    4. feladat

--- jelado.py
+++ jelado.py
@@ -1,16 +1,25 @@
+import math
+
 class Jel:
     def __init__(self, o, p, mp, x, y):
-        self.o = o
-        self.p = p
-        self.mp = mp
-        self.x = x
-        self.y = y
+        self.o = int(o)
+        self.p = int(p)
+        self.mp = int(mp)
+        self.x = int(x)
+        self.y = int(y)
 
 def eltelt(elso, masodik):
     ido1 = elso[0] * 3600 + elso[1] * 60 + elso[2]
     ido2 = masodik[0] * 3600 + masodik[1] * 60 + masodik[2]
     telt = ido2-ido1
     return telt
+
+def vissza(mp):
+    ora = math.floor(mp / 3600)
+    mp = mp - ora*3600
+    perc = math.floor(mp / 60)
+    mp = mp - perc*60
+    return [str(ora), str(perc), str(mp)]
 
 
 jelek = []
@@ -24,3 +33,11 @@ for sor in fajl:
 print("2. feladat")
 sorszam = int(input("Adja meg a jel sorszámát! ")) - 1
 print(f'x={jelek[sorszam].x} y={jelek[sorszam].y}')
+
+
+elso = jelek[0]
+utolso = jelek[-1]
+print('\n4. feladat')
+kozott = eltelt([elso.o, elso.p, elso.mp], [utolso.o, utolso.p, utolso.mp])
+print("Időtartam:", ':'.join(vissza(kozott)))
+
